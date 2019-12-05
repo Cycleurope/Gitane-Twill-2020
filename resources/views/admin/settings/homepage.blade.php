e15ea3f5bdca50ae885f04d3ae57502510ffef1e
Vincent Lombard <vincent.lombard@me.com>
2019-12-05 23:06:35 +0100

    Update homepage.blade.php

--- resources/views/admin/settings/homepage.blade.php
+++ resources/views/admin/settings/homepage.blade.php
@@ -4,15 +4,15 @@
 
 @section('contentFields')
 
-    @formField('block_editor', [
-        'blocks' => [
-            'text', 'left_image_right_content', 
-            'left_content_right_image', 
-            'one_column_image_with_label', 
-            'two_columns_images_with_labels', 
-            'fullwidth_image_with_label', 
-            'youtube_video'
-            ]
-    ])
+@formField('block_editor', [
+    'blocks' => [
+        'text', 'left_image_right_content', 
+        'left_content_right_image', 
+        'one_column_image_with_label', 
+        'two_columns_images_with_labels', 
+        'fullwidth_image_with_label', 
+        'youtube_video'
+        ]
+])
 
 @endsection

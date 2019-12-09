e7454d3aa90c8e1a5578513c8fca2db27a2e6780
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-09 13:56:57 +0100

    Update Page.php

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,67 +2,53 @@
 
 namespace App\Models  ;
 
-use A17\Twill\Models\Behaviors\HasBlocks  ;
-use A17\Twill\Models\Behaviors\HasTranslation  ;
-use A17\Twill\Models\Behaviors\HasSlug  ;
-use A17\Twill\Models\Behaviors\HasMedias  ;
-use A17\Twill\Models\Model  ;
+use A17\Twill\Models\Behaviors\HasBlocks;
+use A17\Twill\Models\Behaviors\HasTranslation;
+use A17\Twill\Models\Behaviors\HasSlug;
+use A17\Twill\Models\Behaviors\HasMedias;
+use A17\Twill\Models\Model;
 
 class Page extends Model
 {
-    use HasBlocks, HasTranslation, HasSlug, HasMedias  ;
+    use HasBlocks, HasTranslation, HasSlug, HasMedias;
 
     protected $fillable = [
-        'published' ,
-        'desktop_cover' ,
-        'tablet_cover' ,
-        'mobile_cover' ,
-        'title' ,
-        'description' ,
-        'position' ,
-        // 'public' ;
-        // 'featured' ;
-        // 'publish_start_date' ;
-        // 'publish_end_date' ;
-    ]  ;
+        'published',
+        'desktop_cover',
+        'tablet_cover',
+        'mobile_cover',
+        'title',
+        'description',
+        'position',
+    ];
 
     // uncomment and modify this as needed if you use the HasTranslation trait
     public $translatedAttributes = [
-        'title' ,
-        'description' ,
-        'active' ,
-        'content' ,
-    ]  ;
+        'title',
+        'description',
+        'active',
+        'content',
+    ];
     
     // uncomment and modify this as needed if you use the HasSlug trait
     public $slugAttributes = [
         'title' ,
-    ]  ;
+    ];
 
     // add checkbox fields names here (published toggle is itself a checkbox)
     public $checkboxes = [
         'published'
-    ]  ;
+    ];
 
     // uncomment and modify this as needed if you use the HasMedias trait
-    // public $mediasParams = [
-    //     'cover' => [
-    //         'default' => [
-    //             [
-    //                 'name' => 'landscape' ;
-    //                 'ratio' => 16 / 9 ;
-    //             ] ;
-    //             [
-    //                 'name' => 'portrait' ;
-    //                 'ratio' => 3 / 4 ;
-    //             ] ;
-    //         ] ;
-    //         'mobile' => [
-    //             [
-    //                 'name' => 'mobile' ;
-    //                 'ratio' => 1 ;
-    //             ] ;
-    //         ] ;
-    //     ] ;
-    // ]  ;
+    public $mediasParams = [
+        'cover' => [
+            'desktop' => [
+                [
+                    'name' => 'landscape',
+                    'ratio' => 1800 / 540,
+                ],
+            ],
+        ],
+    ];
 }

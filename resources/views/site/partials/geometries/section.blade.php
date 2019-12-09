596321616168326b31224d55a0e6ba77709e500d
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-09 10:10:35 +0100

    Update section.blade.php

--- resources/views/site/partials/geometries/section.blade.php
+++ resources/views/site/partials/geometries/section.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12">
             <h2 class="text-uppercase">@lang('bike.geometry.plural')</h2>
         </div>
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-lg-6" style="background-color:#111114;">
             <img src="{{ $bike->geometries->first()->image('picture', 'default', ['w' => 1200]) }}" alt="">
         </div>
         <div class="col-12 col-lg-6">

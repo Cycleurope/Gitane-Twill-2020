8828c47d670f7a84a1e547d2204dc51d99e2b60f
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-10 10:36:49 +0100

    Modification du DOM

---
+++ resources/views/site/partials/stores/international.blade.php
@@ -0,0 +1,8 @@
+<div>
+    @include('site.partials.stores.european_stores')
+    @include('site.partials.stores.northamerican_stores')
+    @include('site.partials.stores.southamerican_stores')
+    @include('site.partials.stores.african_stores')
+    @include('site.partials.stores.asian_stores')
+    @include('site.partials.stores.oceanian_stores')
+</div>
--- resources/views/site/stores.blade.php
+++ resources/views/site/stores.blade.php
@@ -20,11 +20,6 @@
 </div>
 
 @include('site.partials.stores.french_stores')
-@include('site.partials.stores.european_stores')
-@include('site.partials.stores.northamerican_stores')
-@include('site.partials.stores.southamerican_stores')
-@include('site.partials.stores.african_stores')
-@include('site.partials.stores.asian_stores')
-@include('site.partials.stores.oceanian_stores')
+@include('site.partials.stores.international')
 
 @endsection

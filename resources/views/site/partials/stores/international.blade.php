73c69801da48957b7495a5c7b2b73a9eefe54c67
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-10 10:37:43 +0100

    Update international.blade.php

--- resources/views/site/partials/stores/international.blade.php
+++ resources/views/site/partials/stores/international.blade.php
@@ -1,4 +1,11 @@
 <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Gitane dans le monde entier...</h2>
+            </div>
+        </div>
+    </div>
     @include('site.partials.stores.european_stores')
     @include('site.partials.stores.northamerican_stores')
     @include('site.partials.stores.southamerican_stores')

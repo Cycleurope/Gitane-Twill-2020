493ccf9fcbe89a46a8b35da07ffe7e1c54835759
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-11 11:09:55 +0100

    Update cover.blade.php

--- resources/views/site/partials/family/cover.blade.php
+++ resources/views/site/partials/family/cover.blade.php
@@ -2,7 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 px-0">
-                <div class="cover-picture"><img src="{{ $family->image('cover', 'desktop') }}" alt="" width="100%"></div>
+                <div class="cover-picture">
+                    <img src="{{ $family->image('cover', 'desktop') }}" alt="" width="100%" class="d-none d-lg-block">
+                    <img src="{{ $family->image('cover', 'tablet') }}" alt="" width="100%" class="d-none d-md-block d-lg-none">
+                    <img src="{{ $family->image('cover', 'mobile') }}" alt="" width="100%" class="d-block d-md-none">
+                </div>
                 <div class="cover-overlay"></div>
                 <div class="cover-label">
                     <h1><span>{{ $family->translate(app()->getLocale())->title }}</span></h1>

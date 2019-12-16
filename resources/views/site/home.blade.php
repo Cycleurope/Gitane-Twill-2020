5825cbffa7c7be857a0a4ccbd9c42d820e370371
Vincent Lombard <vincent.lombard@me.com>
2019-12-17 00:02:17 +0100

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,5 +2,8 @@
 @section('pageTitle', ' - L\'univers Gitane')
 @section('content')
 
+@include('site.partials.home.featured')
+@include('site.partials.home.familiesslider')
+@include('site.partials.find-a-dealer')
 
 @endsection

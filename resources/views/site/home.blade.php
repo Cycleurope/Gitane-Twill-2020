38c81a3d4b45fee0e529d335b6ba867151a61a7c
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-17 09:09:58 +0100

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,7 +4,6 @@
 
 @include('site.partials.home.homeslider')
 @include('site.partials.home.featured')
-@include('site.partials.home.familiesslider')
-@include('site.partials.stores.find_a_dealer')
+
 
 @endsection

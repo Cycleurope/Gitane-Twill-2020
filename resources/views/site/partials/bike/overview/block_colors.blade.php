6054e8a12e2ee68936587a4329a02050379d9372
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-17 12:02:57 +0100

    Update block_colors.blade.php

--- resources/views/site/partials/bike/overview/block_colors.blade.php
+++ resources/views/site/partials/bike/overview/block_colors.blade.php
@@ -5,7 +5,7 @@
         <ul class="colors-list">
             @foreach($bike->colors as $c)
             <li class="color-item col-10">
-                <span class="picto"><img src="{{ $c->image('color_picture', 'default', ['bg' => '000A']) }}" alt="" height="20"></span> {{ $c->translate(app()->getLocale())->color }}
+                <span class="picto"><img src="{{ $c->image('color_picture', 'default', ['bg' => '400A']) }}" alt="" height="20"></span> {{ $c->translate(app()->getLocale())->color }}
             </li>
             @endforeach
         </ul>

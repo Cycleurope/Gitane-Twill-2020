55a286cddd340542194a1cb1f8673d2e04b001d2
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2019-12-24 11:53:29 +0100

    Update download-book.blade.php

--- resources/views/site/partials/family/download-book.blade.php
+++ resources/views/site/partials/family/download-book.blade.php
@@ -1,7 +1,6 @@
 <section id="download-book" class="py-5">
     <div class="container">
         <div class="row">
-            row
             @if($family->file('book', app()->getLocale()) != '')
             <a href="{{ $family->file('book', app()->getLocale()) }}" target="_blank" download class="col-12 bigbutton bugbutton-blue"><i class="fa fa-file-pdf"></i>&nbsp; Télécharger le catalogue</a>
             @endif

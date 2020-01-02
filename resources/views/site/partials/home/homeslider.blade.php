0043ebe4faafc57b22fcdf32da10b6d24bc04aa1
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-01-02 09:00:28 +0100

    Changement du menu (CSS) + Slider Home

--- resources/views/site/partials/home/homeslider.blade.php
+++ resources/views/site/partials/home/homeslider.blade.php
@@ -2,8 +2,7 @@
 <div id="home-slider" class="owl-carousel">
     @foreach($slides as $slide)
     <div class="slide">
-        <img src="{{ $slide->image('mobile_slide', 'default') }}" alt="" class="d-block d-sm-none">
-        <img src="{{ $slide->image('square_slide', 'default') }}" alt="" class="d-none d-sm-block d-lg-none">
+        <img src="{{ $slide->image('square_slide', 'default') }}" alt="" class="d-block d-lg-none">
         <img src="{{ $slide->image('tablet_slide', 'default') }}" alt="" class="d-none d-lg-block d-xl-none">
         <img src="{{ $slide->image('desktop_slide', 'default') }}" alt="" class="d-none d-xl-block">
         <div class="overlay"></div>
@@ -12,7 +11,9 @@
                 <div class="row">
                     @if($slide->translate(app()->getLocale())->title != "")
                     <div class="col-12 mb-5">
-                        <span class="heading">{{ $slide->translate(app()->getLocale())->title }}</span>
+                        <div class="heading-container">
+                            <span class="heading">{{ $slide->translate(app()->getLocale())->title }}</span>
+                        </div>
                     </div>
                     @endif
                     @if($slide->translate(app()->getLocale())->link != "")

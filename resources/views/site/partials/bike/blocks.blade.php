0950f68dfb241cbc123f637d2eca4b58564a4280
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-01-06 15:00:21 +0100

    Update blocks.blade.php

--- resources/views/site/partials/bike/blocks.blade.php
+++ resources/views/site/partials/bike/blocks.blade.php
@@ -1,5 +1,5 @@
 @if(count($bike->blocks) > 0)
-<section id="product-features" class="py-0">
+<section id="product-features" class="py-0" style="border-top: 1px solid #f4f4f4;">
     <div class="container-fluid">
         <div class="row">
             <div class="col">

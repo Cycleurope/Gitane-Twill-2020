ceceb474d3a49d576c08150672e1767e467b2c57
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-01-06 14:39:23 +0100

    Update left_content_right_image.blade.php

--- resources/views/site/blocks/left_content_right_image.blade.php
+++ resources/views/site/blocks/left_content_right_image.blade.php
@@ -2,9 +2,12 @@
     <div class="col-12 col-md-6 col-lg-6 d-block d-md-none px-0"><img src="{{ $block->image('picture') }}" alt=""></div>
     <div class="col-12 col-md-6 info text-right">
         <div class="px-5 py-5">
-            <h3 class="left-bordered">Lorem ipsum dolor sit ; amet consectetur adipisicing elit. Quas ; porro.</h3>
+            @if($block->translatedInput('side_title') != '')
+            <h3 class="left-bordered">{{ $block->translatedinput('side_title') }}</h3>
+            @endif
             {{ $block->translatedinput('side_content') }}
         </div>
+        
     </div>
     <div class="col-12 col-md-6 col-6 px-0 d-none d-md-block cover" style="background-image: url({{ $block->image('picture') }})  ; background-size: cover  ;"></div>
 </div>

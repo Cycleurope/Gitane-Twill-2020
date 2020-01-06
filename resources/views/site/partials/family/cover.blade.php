a21727083c9ad747337c3e0422f3a46a06cb27d3
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-01-06 11:38:26 +0100

    Update cover.blade.php

--- resources/views/site/partials/family/cover.blade.php
+++ resources/views/site/partials/family/cover.blade.php
@@ -15,7 +15,13 @@
                 </div>
                 <div class="cover-overlay"></div>
                 <div class="cover-label">
-                    <h1><span>{{ $family->translate(app()->getLocale())->title }}</span></h1>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1><span>{{ $family->translate(app()->getLocale())->title }}</span></h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

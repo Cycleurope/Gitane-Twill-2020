c0acf33ce23fbcace34d43905ba5649ef81b5c74
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-01-09 17:17:58 +0100

    Update cover.blade.php

--- resources/views/site/partials/bike/cover.blade.php
+++ resources/views/site/partials/bike/cover.blade.php
@@ -2,7 +2,17 @@
 <div id="cover" class="container-fluid">
     <div class="row">
         <div class="col-12 px-0">
-            <div class="cover-picture"><img src="{{ $bike->image('desktop_cover', 'default') }}" alt="" width="100%"></div>
+            <div class="cover-picture">
+                @if($bike->hasImage('cover', 'desktop'))
+                <img src="{{ $bike->image('cover', 'desktop') }}" alt="" width="100%" class="d-none d-lg-block">
+                @endif
+                @if($bike->hasImage('cover', 'tablet'))
+                <img src="{{ $bike->image('cover', 'tablet') }}" alt="" width="100%" class="d-none d-md-block d-lg-none">
+                @endif
+                @if($bike->hasImage('cover', 'mobile'))
+                <img src="{{ $bike->image('cover', 'square') }}" alt="" width="100%" class="d-block d-md-none">
+                @endif
+            </div>
             <div class="cover-overlay"></div>
         </div>
     </div>
